6856abcaae4e25d53ea7c128267ec6bbef982a48
Elke Heymans <el.heymans@gmail.com>
2025-10-25 09:17:49 +0200

    Day 04 part 2

--- days/04/test_04.py
+++ days/04/test_04.py
@@ -19,8 +19,8 @@ def find_checksum_letter_in_name(name: str, checksum_letter: str):
     return -1
 
 
-def calculate_sector_id(name_to_parse: str) -> int:
-    (name, sector_id, checksum) = parse(name_to_parse)
+def calculate_sector_id(data: tuple[str, int, str]) -> int:
+    name, sector_id, checksum = data
     occurences = {}
     for letter in name:
         occurences[letter] = occurences.get(letter, 0) + 1
@@ -40,15 +40,75 @@ def test_part_1_example():
         "not-a-real-room-404[oarel]",
         "totally-real-room-200[decoy]",
     ]
-    assert calculate_sector_id("aaaaa-bbb-z-y-x-123[abxyz]") == 123
-    assert calculate_sector_id("a-b-c-d-e-f-g-h-987[abcde]") == 987
-    assert calculate_sector_id("not-a-real-room-404[oarel]") == 404
-    assert calculate_sector_id("totally-real-room-200[decoy]") == 0
-    assert sum(map(calculate_sector_id, lines)) == 1514
+    assert calculate_sector_id(parse("aaaaa-bbb-z-y-x-123[abxyz]")) == 123
+    assert calculate_sector_id(parse("a-b-c-d-e-f-g-h-987[abcde]")) == 987
+    assert calculate_sector_id(parse("not-a-real-room-404[oarel]")) == 404
+    assert calculate_sector_id(parse("totally-real-room-200[decoy]")) == 0
+    assert sum(map(calculate_sector_id, map(parse, lines))) == 1514
 
 
 def test_part_1_actual():
     with open("days/04/input.txt") as f:
         input_data = f.read()
     lines = input_data.strip().split("\n")
-    assert sum(map(calculate_sector_id, lines)) == 158835
+    assert sum(map(calculate_sector_id, map(parse, lines))) == 158835
+
+
+def rotate_letter(letter: str) -> str:
+    match letter:
+        case "z":
+            return "a"
+        case "-":
+            return " "
+        case " ":
+            return " "
+        case _:
+            return chr(ord(letter) + 1)
+
+
+def rotate_name(name: str, sector_id) -> str:
+    for _ in range(sector_id):
+        name = map(rotate_letter, name)
+    return "".join(name)
+
+
+def parse_with_rotation(line: str) -> tuple[str, int, str]:
+    (name, sector_id, checksum) = parse(line)
+    return (rotate_name(name, sector_id), sector_id, checksum)
+
+
+def test_rotate_letter():
+    assert rotate_letter("z") == "a"
+    assert rotate_letter("a") == "b"
+    assert rotate_letter("b") == "c"
+    assert rotate_letter("-") == " "
+    assert rotate_letter(" ") == " "
+
+
+def test_rotate_name():
+    assert rotate_name("qzmt-zixmtkozy-ivhz", 343) == "very encrypted name"
+
+
+def test_part_2_example():
+    lines = [
+        "aaaaa-bbb-z-y-x-123[abxyz]",
+        "a-b-c-d-e-f-g-h-987[abcde]",
+        "not-a-real-room-404[oarel]",
+        "totally-real-room-200[decoy]",
+    ]
+    assert sum(map(calculate_sector_id, map(parse_with_rotation, lines))) == 987
+
+
+def test_part_2_actual():
+    with open("days/04/input.txt") as f:
+        input_data = f.read()
+    lines = input_data.strip().split("\n")
+    assert (
+        list(
+            filter(
+                lambda x: x[0] == "northpoleobjectstorage",
+                map(parse_with_rotation, lines),
+            )
+        )[0][1]
+        == 993
+    )
